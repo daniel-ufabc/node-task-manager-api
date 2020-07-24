d70db97a83e37fb2f1a5a3b50b001cfa59fb3d73
Daniel M. Martin <danielmmartin@gmail.com>
2020-07-24 18:28:57 -0300

    add test route

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/test', (req, res) => {
+    res.send({ message: 'Site up and running.' })
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
